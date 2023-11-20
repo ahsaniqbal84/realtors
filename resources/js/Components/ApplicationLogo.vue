221a42f9876796e06b95c655b6132fc0f6d3b966
Ahsan Iqbal <ahsan.iqbal5219@gmail.com>
2023-11-20 10:00:48 +0500

    included routes

--- resources/js/Components/ApplicationLogo.vue
+++ resources/js/Components/ApplicationLogo.vue
@@ -1,6 +1,4 @@
 <template>
     <!-- Assuming "logo.jpg" is the name of your JPEG image file -->
-    <div>
-        <img src="@/assets/logo.JPG" alt="Application Logo" />
-    </div>
+    <div>Logo</div>
 </template>
